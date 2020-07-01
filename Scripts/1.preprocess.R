d2e76073283ab65c4c0ed2073dfdd3601bdc4e30
Dimitrios KYRIAKIS <dimitrios.kyriakis@uni.lu>
2020-07-01 17:27:14 +0200

    All data

--- Scripts/1.preprocess.R
+++ Scripts/1.preprocess.R
@@ -5,6 +5,7 @@ date: "26/6/2020"
 ---
 
 # ============================== Libraries ========================================
+library(HDF5Array)
 library(BiocParallel)
 library(tidyverse)
 library(tictoc)
@@ -45,8 +46,15 @@ filenames_new <-list.files(path_new,full.names = TRUE)
 filenames_DGE_new <- filenames_new[grep("Summary",filenames_new,invert=TRUE)][c(1,2)]
 cond_names_new <- c("T470S","T101S")
 
+
+
+
 filenames <- c(filenames_DGE_old,filenames_DGE_new)
 condition_names <- c(cond_names_old,cond_names_new)
+
+old_suresh <- list.files("/home/users/dkyriakis/PhD/Projects/Yahaya/DATA_old",full.names=T)
+filenames <- c(filenames,old_suresh)
+condition_names <- c(condition_names,c("Control","p13_old","p3_old","p8"))
 # -----------------------------------------------------------------------------------
 
 
@@ -54,7 +62,7 @@ condition_names <- c(cond_names_old,cond_names_new)
 feat.mad <- 4
 count.mad <- 4
 mito.mad <- 4
-n_cores <- 2
+n_cores <- 3
 if (get_os()=="windows"){
     bpparam <- BiocParallel::SnowParam(workers = n_cores, type = "SOCK")
 }else{
@@ -66,29 +74,19 @@ if (get_os()=="windows"){
 dir.create("1.QC")
 setwd("1.QC")
 tm_list <- create_seurat(filenames = filenames ,conditions = condition_names,
-                         elbow = TRUE,data_10x = FALSE,n_cores=n_cores)
+                         elbow = TRUE,data_10x = FALSE,n_cores=1)
 setwd("../")
 # ---------------------------------------------------------------------------------
 
 # ================================= Filtering ======================================
 dir.create("2.Filtering")
 setwd("2.Filtering")
-tm_list_fl <- filtering_diy(tm_list,feat.mad = 2,count.mad = 2,mito.mad = 1.5,n_cores = 4)
+tm_list_fl <- filtering_diy(tm_list,feat.mad = 2,count.mad = 2,mito.mad = 1.5,n_cores = 1)
 setwd("../")
 # ----------------------------------------------------------------------------------
+save.image("Filtered.RData")
 
 # ================================= Projection ======================================
-dir.create("3.Projection")
-setwd("3.Projection")
-test <- SCT_projection(tm_list_fl)
-pdf("Projections.pdf")
-FeaturePlot(test, c("PLP1","P2RY12","GJA1","OLIG1","GAD2","IGF2","CCDC153"),order=TRUE,cols = c("lightgrey","#FDBB84","#EF6548","#D7301F","#B30000","#7F0000"))
-dev.off()
-pdf("Cells.pdf",width=12)
-DimPlot(test,group.by = "condition",cols=color_cond)
-dev.off()
-setwd("../")
-
 dir.create("3.Projection_nobatch")
 setwd("3.Projection_nobatch")
 test <- nobatch_projection(tm_list_fl)
@@ -101,8 +99,30 @@ dev.off()
 setwd("../")
 
 
-dir.create("3.Clustering")
-setwd("3.Clustering")
+require(liger)
+require(SeuratWrappers)
+test <-liger_projection(test,split.by="condition",k=20,lamda=5,n_neighbors=10)
+
+
+dir.create("3.Projection")
+setwd("3.Projection")
+test <- SCT_projection(tm_list_fl)
+
+pdf("Markers.pdf")
+FeaturePlot(test, c("PLP1","P2RY12","GJA1","OLIG1","GAD2","IGF2","CCDC153"),
+    order=TRUE,cols = c("lightgrey","#FDBB84","#EF6548","#D7301F","#B30000","#7F0000"),reduction="liger_umap")
+dev.off()
+
+pdf("Cells.pdf",width=12)
+DimPlot(test,group.by = "condition",cols=color_cond,reduction="liger_umap")
+dev.off()
+setwd("../")
+# ----------------------------------------------------------------------------------
+
+# ================================= Clustering ======================================
+
+dir.create("4.Clustering")
+setwd("4.Clustering")
 source('/home/users/dkyriakis/PhD/Projects/Yahaya/Script_Library/Clustering.R')
 library("cluster")
 library("clustree")
@@ -116,11 +136,15 @@ test <- FindClusters(test, resolution = seq(0,1.1,0.1))
 
 clustree <-clustree(test,prefix="integrated_snn_res.")
 # clustree <-clustree(test,prefix="SCT_snn_res.")
-
+test$Cluster <- test$Liger_Clusters
 pdf("Clustering.pdf")
-plot(clustree)
-DimPlot(test,group.by = c("Cluster","Phase"))
+# plot(clustree)
+DimPlot(test,group.by = c("Cluster"),reduction="liger_umap",cols=color_clust)
+DimPlot(test,group.by = c("Phase"),reduction="liger_umap")
 dev.off()
 # ---------------------------------------------------------------------------
 graphics.off()
 setwd("../")
+
+
+save.image("All_data.RData")
